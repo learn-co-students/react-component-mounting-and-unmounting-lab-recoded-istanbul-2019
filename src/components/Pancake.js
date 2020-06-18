35a1230eca3970197b192284e98201e9c4965575
Hassan <github email address>
2020-06-18 11:27:26 +0300

    Done.

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -6,17 +6,25 @@ class Pancake extends React.Component {
 
     this.state = {
       timeCooked: 0,
-      flippedAt: undefined
+      flippedAt: undefined,
     };
   }
 
-  // TODO: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
+  // DONE: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
 
-  // TODO: create a componentWillUnmount() which will clear the interval
+  componentDidMount() {
+    this.startInterval();
+  }
+
+  // DONE: create a componentWillUnmount() which will clear the interval
+
+  componentWillUnmount() {
+    this.cleanUpInterval();
+  }
 
   updateCounter = () => {
     this.setState({
-      timeCooked: this.state.timeCooked + 1
+      timeCooked: this.state.timeCooked + 1,
     });
   };
 
@@ -30,7 +38,7 @@ class Pancake extends React.Component {
 
   flip = () => {
     this.setState({
-      flippedAt: this.state.timeCooked
+      flippedAt: this.state.timeCooked,
     });
   };
 
